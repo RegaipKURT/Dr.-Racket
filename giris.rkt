4a83fb826ef15d6d58c421e9782ffde2a09d9df7
Regaip KURT <regkrt@gmail.com>
2018-08-19 05:16:34 +0300

    Update giris.rkt

--- giris.rkt
+++ giris.rkt
@@ -1,7 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname giris) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+
 #! lang racket
+#! Student Advanced dilini seçmediğiniz takdirde kodların ileri kısımlarında hata verecektir.
 
 (define code "Kod: ") #! define değişken tanımlamaya yarar
 (define name "PARS")
